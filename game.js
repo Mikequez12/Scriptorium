6c860045ab34d2ca8ccbeb8cb1ec80f332714b73
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-03-25 13:06:04 +0100

    update title

--- game.js
+++ game.js
@@ -204,7 +204,7 @@ document.addEventListener('keydown',(event) => {
 async function startGame() {
     game.lang = document.querySelector('select#lang').value;
 
-    document.head.appendChild(document.createElement('title')).textContent = game.name;
+    document.querySelector('title').textContent = game.name;
 
     game.player = {};
     game.player.sex = document.querySelector('select#sex').value;
