85e26f9627e45bb221b157a121846ade27bcebde
Markel <142720633+Mikequez12@users.noreply.github.com>
2025-04-06 00:18:10 +0200

    update good version

--- fast.js
+++ fast.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded',(DOMEvent) => {
     if (window.location.hash != '#fast') {
         return
@@ -11,18 +10,4 @@ document.addEventListener('DOMContentLoaded',(DOMEvent) => {
         document.querySelector('#censor').checked = false;
         startGame();
     }, 500);
-=======
-document.addEventListener('DOMContentLoaded',(DOMEvent) => {
-    if (window.location.hash != '#fast') {
-        return
-    }
-    // GO DIRECTLY TO VANILLA //
-    gameConfigToplevel();
-    findGame({target:document.querySelector('button#search')});
-    setTimeout(() => {
-        document.querySelector('#name').value = 'Mike';
-        document.querySelector('#censor').checked = false;
-        startGame();
-    }, 500);
->>>>>>> aed125db1f4afa5ce004d40bd68feabb68a684a5
 });
